aeed41e7d46c1c6446270bbf94efeeb445f1174e
jidon-07 <jidon3005m@gmail.com>
2026-05-05 08:15:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'funfacts765@gmail.com' 
-app.config['MAIL_PASSWORD'] = 'mohu xeye wxlk tbps' 
+app.config['MAIL_PASSWORD'] = 'stdt ijlu qyqz amai' 
 mail = Mail(app)
 db = SQLAlchemy(app)
 
